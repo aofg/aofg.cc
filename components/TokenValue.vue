7b83733af8548450ee7c3d215fef2228cee146ef
Aler Denisov <aler.zampillo@gmail.com>
2018-09-26 22:36:39 +0200

    typescript initial

---
+++ components/TokenValue.vue
@@ -0,0 +1,74 @@
+<template lang="pug">
+  span(:class='b()')
+    span(:class='b("round")') {{ round }}
+    span(:class='b("dot")') {{ dot }}
+    span(:class='b("decimals")') {{ shownDecimals }}
+    span(:class="b('ticker')") {{ tiker }}
+</template>
+
+<style lang="scss">
+.token-value {
+  // text-align: right;
+  display: flex;
+  font-variant-numeric: tabular-nums;
+
+  &__round {
+    // flex-grow: 1;
+  }
+
+  &__decimals {
+    font-size: 80%;
+    opacity: 0.6;
+  }
+
+  &__ticker {
+    width: 60px;
+    text-align: left;
+    margin-left: 0.45em;
+  }
+}
+</style>
+
+
+<script lang="ts">
+import { Component, Vue, Prop } from "nuxt-property-decorator";
+
+@Component({
+  name: "token-value"
+})
+export default class extends Vue {
+  @Prop({ required: true })
+  value: string;
+
+  @Prop({ default: "MNC" })
+  tiker: string;
+
+  @Prop({ default: 18 })
+  decimals: number;
+
+  @Prop({ default: 4 })
+  showDecimals: number;
+
+  @Prop({ default: "." })
+  dot: string;
+
+  @Prop({ default: false })
+  delimited: string;
+
+  get normalized() {
+    return this.value.padStart(19, "0");
+  }
+  get fullDecimals() {
+    return this.normalized.slice(-18);
+  }
+
+  get shownDecimals() {
+    return this.fullDecimals.slice(0, this.showDecimals);
+  }
+
+  get round() {
+    return this.normalized.slice(0, -18);
+  }
+}
+</script>
+
